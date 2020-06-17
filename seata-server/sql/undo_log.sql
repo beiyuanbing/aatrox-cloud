d7907bc44953aafc29112369778dbaf734437abd
beiyuanbing <425210220@qq.com>
2020-06-17 17:27:52 +0800

    增加undo.log

---
+++ seata-server/sql/undo_log.sql
@@ -0,0 +1,18 @@
+-- auto-generated definition
+create table undo_log
+(
+    id            bigint auto_increment
+        primary key,
+    branch_id     bigint       not null,
+    xid           varchar(100) not null,
+    context       varchar(128) not null,
+    rollback_info longblob     not null,
+    log_status    int          not null,
+    log_created   datetime     not null,
+    log_modified  datetime     not null,
+    ext           varchar(100) null,
+    constraint ux_undo_log
+        unique (xid, branch_id)
+)
+    charset = utf8;
+
